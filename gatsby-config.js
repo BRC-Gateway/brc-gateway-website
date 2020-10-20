b5dcedd0dde4514d742f66064005f04b65edb87d
Chris <chris.c.thomas@icloud.com>
2020-10-20 16:06:17 -0500

    Add PostCSS and plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   pathPrefix: "brc-gateway-website",
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
