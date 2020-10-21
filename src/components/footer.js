84796bd3296596ee599ef6c7590c23e8aa89f0ab
Chris <chris.c.thomas@icloud.com>
2020-10-21 10:44:25 -0500

    Updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,14 @@ import React from "react"
 import BRCLogo from "../images/brc-logo.png"
 
 const Footer = ({ siteTitle }) => (
-  <footer style={{ marginTop: '200px'}}>
+  <footer style={{ marginTop: '200px' }}>
     <h2 style={{ textAlign: 'center' }}>BRC Gateway Footer</h2>
     <div style={{
       display: 'flex',
       justifyContent: 'space-between',
     }}>
       <p>NIAID Contract Info</p>
-      <img src={BRCLogo} alt="BRC Logo" style={{ height: '100px' }}/>
+      <img src={BRCLogo} alt="BRC Logo" style={{ height: '100px' }} />
       <p>Social Media Links</p>
     </div>
   </footer>
--- src/components/header.js
+++ src/components/header.js
@@ -3,38 +3,19 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: 'flex',
-        alignItems: 'baseline',
-        justifyContent: 'space-between',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-            flexGrow: '4',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <div class="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/contact">Contact Us</Link>
+  <header>
+    <div className="container mx-auto">
+      <div className="flex justify-between items-baseline">
+        <h1>
+          <Link to="/">
+            {siteTitle}
+          </Link>
+        </h1>
+        <div class="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+        </div>
       </div>
     </div>
   </header>
