7330f497df53875d76d0be457e86c4b1e003920a
Chris <chris.c.thomas@icloud.com>
2020-11-02 10:46:45 -0600

    Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -201,40 +201,39 @@ const IndexPage = () => (
                 </div>
               </div>
             </div>
+          </div>
 
-            <div class="col-md-4 col-lg-3 news-col mt-4">
-              <h5>Event Calendar</h5>
-              <div class="scrl-box">
-                <div class="home-block">
-                  <h6 class="mb-1"><a>Upcoming Event</a></h6>
-                  <p>Join us on September 11, 2019 at 10:00 am (EST) for our Release 45 Highlights webinar where we will discuss... <a href="">read more</a></p>
-                </div>
-                <div class="home-block">
-                  <h6 class="mb-1"><a>Another Upcoming Event</a></h6>
-                  <p>Join us on July 12th at 10:00 am (EST) for our Release 44 Highlights webinar where we will discuss... <a href="">read more</a></p>
-                </div>
-                <div class="home-block">
-                  <h6 class="mb-1"><a>One More Upcoming Event</a></h6>
-                  <p>Join us on May 15th at 10:00 am (EST) for our Release 43 Highlights webinar where we will discuss... <a href="">read more</a></p>
-                </div>
+          <div class="col-md-4 col-lg-3 news-col mt-4">
+            <h5>Event Calendar</h5>
+            <div class="scrl-box">
+              <div class="home-block">
+                <h6 class="mb-1"><a>Upcoming Event</a></h6>
+                <p>Join us on September 11, 2019 at 10:00 am (EST) for our Release 45 Highlights webinar where we will discuss... <a href="">read more</a></p>
               </div>
-
-              <div class="home-block mt-4">
-                <a href="data.html" class="btn btn-outline-dark rounded-pill">All Upcoming Events &raquo;</a>
+              <div class="home-block">
+                <h6 class="mb-1"><a>Another Upcoming Event</a></h6>
+                <p>Join us on July 12th at 10:00 am (EST) for our Release 44 Highlights webinar where we will discuss... <a href="">read more</a></p>
               </div>
-              <hr />
-              <h5>Tweets <span>by <a href="#">@BV-BRC</a> <a href="#">@VEuPathDB</a></span></h5>
-              <div class="scrl-box">
-                <img src={Tweet1} class="img-fluid" />
-                <img src={Tweet2} class="img-fluid" />
-              </div>
-              <div class="home-block mt-4">
-                <a href="data.html" class="btn btn-outline-dark rounded-pill">View on Twitter &raquo;</a>
+              <div class="home-block">
+                <h6 class="mb-1"><a>One More Upcoming Event</a></h6>
+                <p>Join us on May 15th at 10:00 am (EST) for our Release 43 Highlights webinar where we will discuss... <a href="">read more</a></p>
               </div>
             </div>
 
-
+            <div class="home-block mt-4">
+              <a href="data.html" class="btn btn-outline-dark rounded-pill">All Upcoming Events &raquo;</a>
+            </div>
+            <hr />
+            <h5>Tweets <span>by <a href="#">@BV-BRC</a> <a href="#">@VEuPathDB</a></span></h5>
+            <div class="scrl-box">
+              <img src={Tweet1} class="img-fluid" />
+              <img src={Tweet2} class="img-fluid" />
+            </div>
+            <div class="home-block mt-4">
+              <a href="data.html" class="btn btn-outline-dark rounded-pill">View on Twitter &raquo;</a>
+            </div>
           </div>
+
         </div>
       </div>
     </main>
