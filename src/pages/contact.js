bdce47eb2aa274355edb8b7f076637a4849a89c4
Chris <chris.c.thomas@icloud.com>
2020-11-03 08:56:11 -0600

    Bootstrap contact form sample

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,6 +10,22 @@ const ContactPage = () => (
     <div class="row mb-4 px-3 pt-4 pb-2 bg-wash tw-border-b-4 tw-border-gray-600">
       <div class="col-12">
         <h4>Contact</h4>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Email</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Message</label>
+            <textarea type="textarea" className="form-control" />
+          </div>
+          <button type="submit" class="btn btn-primary">Send</button>
+        </form>
       </div>
     </div>
   </Layout>
