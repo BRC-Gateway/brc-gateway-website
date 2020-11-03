ced894b1720e0230607eec3c402757f77bc1d9c6
Chris <chris.c.thomas@icloud.com>
2020-11-03 08:22:57 -0600

    Contact/Calendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -4,15 +4,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactPage = () => (
+const CalendarPage = () => (
   <Layout>
-    <SEO title="Contact" />
+    <SEO title="About Us" />
     <div class="row mb-4 px-3 pt-4 pb-2 bg-wash tw-border-b-4 tw-border-gray-600">
       <div class="col-12">
-        <h4>Contact</h4>
+        <h4>Calendar</h4>
       </div>
     </div>
   </Layout>
 )
 
-export default ContactPage
+export default CalendarPage
