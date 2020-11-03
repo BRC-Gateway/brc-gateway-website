06b406d2a69ecc659bfe01bd40dc667d21aeaceb
Chris <chris.c.thomas@icloud.com>
2020-11-03 12:41:15 -0600

    Calendar component

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -3,24 +3,7 @@ import React from "react"
 const Calendar = () => (
   <>
     <h5>Event Calendar</h5>
-    <div class="scrl-box">
-      <div class="home-block">
-        <h6 class="mb-1"><a>Upcoming Event</a></h6>
-        <p>Join us on September 11, 2019 at 10:00 am (EST) for our Release 45 Highlights webinar where we will discuss... <a href="">read more</a></p>
-      </div>
-      <div class="home-block">
-        <h6 class="mb-1"><a>Another Upcoming Event</a></h6>
-        <p>Join us on July 12th at 10:00 am (EST) for our Release 44 Highlights webinar where we will discuss... <a href="">read more</a></p>
-      </div>
-      <div class="home-block">
-        <h6 class="mb-1"><a>One More Upcoming Event</a></h6>
-        <p>Join us on May 15th at 10:00 am (EST) for our Release 43 Highlights webinar where we will discuss... <a href="">read more</a></p>
-      </div>
-    </div>
-
-    <div class="home-block mt-4">
-      <a href="data.html" class="btn btn-outline-dark rounded-pill">All Upcoming Events &raquo;</a>
-    </div>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=YnJjZ2F0ZXdheUBnbWFpbC5jb20&amp;src=Y19xbG9ncGFwYnViZmh1M2c3cmgzZWsyODlhY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=YnZicmMxQGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;mode=AGENDA" width="300" height="325" frameborder="0" scrolling="no"></iframe>
   </>
 )
 
