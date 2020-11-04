b6ecd4af50a6f1640c06e1b8ae8560a7ee8cfe0a
Chris <chris.c.thomas@icloud.com>
2020-11-04 11:31:04 -0600

    Remove calendar styling

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -8,10 +8,10 @@ const CalendarPage = () => (
   <Layout>
     <SEO title="Event Calendar" />
     <div class="row mb-4 px-3 pt-4 pb-2 bg-wash tw-border-b-4 tw-border-gray-600">
-      <div class="col-12">
+      <div class="col-md-12">
         <h4>Event Calendar</h4>
 
-        <iframe src="https://calendar.google.com/calendar/u/0/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America/Chicago&src=YnJjZ2F0ZXdheUBnbWFpbC5jb20&src=Y19xbG9ncGFwYnViZmh1M2c3cmgzZWsyODlhY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=YnZicmMxQGdtYWlsLmNvbQ&color=%23039BE5&color=%23F09300&color=%23E4C441&showNav=1&showDate=1&showCalendars=1" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23fffcfc&amp;ctz=America%2FChicago&amp;src=YnJjZ2F0ZXdheUBnbWFpbC5jb20&amp;src=Y19xbG9ncGFwYnViZmh1M2c3cmgzZWsyODlhY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=YnZicmMxQGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;color=%23F09300&amp;color=%23E4C441&amp;showTitle=0&amp;showCalendars=0&amp;showTabs=1&amp;showPrint=0&amp;showDate=0&amp;showNav=0&amp;showTz=0" style={{ borderWidth: '0', }} width="800" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
     </div>
   </Layout>
