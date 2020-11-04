3a7ea8a43b7849a93dd36142823db807c01e45b1
Chris <chris.c.thomas@icloud.com>
2020-11-04 11:36:57 -0600

    Remove calendar styling

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -3,7 +3,7 @@ import React from "react"
 const Calendar = () => (
   <>
     <h5>Event Calendar</h5>
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=YnJjZ2F0ZXdheUBnbWFpbC5jb20&amp;src=Y19xbG9ncGFwYnViZmh1M2c3cmgzZWsyODlhY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=YnZicmMxQGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;mode=AGENDA" width="300" height="325" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=YnJjZ2F0ZXdheUBnbWFpbC5jb20&amp;src=Y19xbG9ncGFwYnViZmh1M2c3cmgzZWsyODlhY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=YnZicmMxQGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;showTitle=0&amp;showCalendars=0&amp;showTabs=1&amp;showPrint=0&amp;showDate=0&amp;showNav=0&amp;showTz=0&amp;mode=AGENDA" style={{ borderWidth: '0', }} width="300" height="325" frameborder="0" scrolling="no"></iframe>
   </>
 )
 
