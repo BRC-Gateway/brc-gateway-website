372bf6c4657acc6eec8fe248f05edd797c14c938
Chris <chris.c.thomas@icloud.com>
2020-11-17 16:40:15 -0600

    change name

--- src/pages/outreach.js
+++ src/pages/outreach.js
@@ -16,7 +16,7 @@ const CalendarPage = () => (
         <a href="https://veupathdb.org/veupathdb/app/static-content/landing.html" target="_blank">VEuPathDB Learning Materials</a><br />
         <br />
         <hr />
-        <h4>Event Calendar</h4>
+        <h4>Outreach Calendar</h4>
 
         <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23fffcfc&amp;ctz=America%2FChicago&amp;src=YnJjZ2F0ZXdheUBnbWFpbC5jb20&amp;src=Y19xbG9ncGFwYnViZmh1M2c3cmgzZWsyODlhY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=YnZicmMxQGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;color=%23F09300&amp;color=%23E4C441&amp;showTitle=0&amp;showCalendars=0&amp;showTabs=1&amp;showPrint=0&amp;showDate=0&amp;showNav=0&amp;showTz=0" style={{ borderWidth: '0', }} width="800" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
