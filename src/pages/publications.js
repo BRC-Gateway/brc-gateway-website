b7e5008737c0e7317c14d19d1104e6dd61dffc11
Chris <chris.c.thomas@icloud.com>
2020-11-23 10:11:43 -0600

    Publications

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -11,7 +11,100 @@ const PublicationsPage = () => (
       <div class="col-12">
         <h4>Publications</h4>
         <p>Citations to the BRC resource as measured Google Scholar searches using predetermined set of keywords based on name and/or acronym of each of the BRC resources, and additional keywords to filter out any false positive or negative results to the extent possible. This is complementary to the citations to the BRC publications described above and necessary because, often, users cite BRC resources by mentioning the resource name or URL in the manuscript text, instead of citing relevant publications.</p>
-        <h4><a href="https://scholar.google.com/citations?user=kXLGwkYAAAAJ&hl=en&authuser=2" target="blank">Google Scholar Citations</a></h4>
+        <h5><a href="https://scholar.google.com/citations?user=kXLGwkYAAAAJ&hl=en&authuser=2" target="blank">Google Scholar Citations</a></h5>
+        <hr />
+        <h6>2020</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=3186439241547682154&btnI=1&hl=en" target="blank">The PATRIC Bioinformatics Resource Center: expanding data and analysis capabilities</a>, JJ Davis, AR Wattam, RK Aziz, T Brettin, R Butler… - Nucleic acids research, 2020</p>
+
+        <hr />
+        <h6>2019</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=3727477073428023507&btnI=1&hl=en" target="blank">Database and Analytical Resources for Viral Research Community</a>, S Phadke, S Macherla, RH Scheuermann - Reference Module in Life Sciences, 2019</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=17231580275753716872&btnI=1&hl=en" target="blank">Hepatitis C virus database and Bioinformatics analysis tools in the virus Pathogen Resource (ViPR)</a>, Y Zhang, C Zmasek, G Sun, CN Larsen… - Hepatitis C Virus Protocols, 2019</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=7483236522122502746&btnI=1&hl=en" target="blank">PATRIC as a unique resource for studying antimicrobial resistance</a>, DA Antonopoulos, R Assaf, RK Aziz, T Brettin, C Bun… - Briefings in bioinformatics, 2019</p>
+
+        <hr />
+        <h6>2018</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=14964473773590748831&btnI=1&hl=en" target="blank">Assembly, annotation, and comparative genomics in PATRIC, the all bacterial bioinformatics resource center</a>, AR Wattam, T Brettin, JJ Davis, S Gerdes, R Kenyon… - Comparative Genomics, 2018</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=16576430230867278864&btnI=1&hl=en" target="blank">EuPathDB: the eukaryotic pathogen genomics database resource</a>, S Warrenfeltz, EY Basenko, K Crouch, OS Harb… - Eukaryotic Genomic Databases, 2018</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=1140902802577723366&btnI=1&hl=en" target="blank">FungiDB: an integrated bioinformatic resource for fungi and oomycetes</a>, EY Basenko, JA Pulman, A Shanmugasundram… - Journal of Fungi, 2018</p>
+
+        <hr />
+        <h6>2017</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=11209962835793954059&btnI=1&hl=en" target="blank">EuPathDB: the eukaryotic pathogen genomics database resource</a>, C Aurrecoechea, A Barreto, EY Basenko, J Brestelli… - Nucleic acids research, 2017</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=778571453403761959&btnI=1&hl=en" target="blank">Improvements to PATRIC, the all-bacterial bioinformatics database and analysis resource center</a>, AR Wattam, JJ Davis, R Assaf, S Boisvert, T Brettin… - Nucleic acids research, 2017</p>
+
+        <hr />
+        <h6>2016</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=3803044944862572916&btnI=1&hl=en" target="blank">Antimicrobial resistance prediction in PATRIC and RAST</a>, JJ Davis, S Boisvert, T Brettin, RW Kenyon, C Mao… - Scientific reports, 2016</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=14397850703049765485&btnI=1&hl=en" target="blank">PATtyFams: protein families for the microbial genomes in the PATRIC database</a>, JJ Davis, S Gerdes, GJ Olsen, R Olson, GD Pusch… - Frontiers in microbiology, 2016</p>
+
+        <hr />
+        <h6>2015</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=4045930543967995705&btnI=1&hl=en" target="blank">Curation, integration and visualization of bacterial virulence factors in PATRIC</a>, C Mao, D Abraham, AR Wattam, MJC Wilson, M Shukla… - Bioinformatics, 2015</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=8868926006119381172&btnI=1&hl=en" target="blank">RASTtk: a modular and extensible implementation of the RAST algorithm for building custom annotation pipelines and annotating batches of genomes</a>, T Brettin, JJ Davis, T Disz, RA Edwards, S Gerdes… - Scientific reports, 2015</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=13162823724022755745&btnI=1&hl=en" target="blank">The Eukaryotic Pathogen Databases: a functional genomic resource integrating data from human and veterinary parasites</a>, OS Harb, DS Roos - Parasite Genomics Protocols, 2015</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=15446404580410887553&btnI=1&hl=en" target="blank">VectorBase: an updated bioinformatics resource for invertebrate vectors and other organisms related with human diseases</a>, GI Giraldo-Calderón, SJ Emrich, RM MacCallum… - Nucleic acids research, 2015</p>
+
+        <hr />
+        <h6>2014</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=9645715506652513748&btnI=1&hl=en" target="blank">The SEED and the Rapid Annotation of microbial genomes using Subsystems Technology (RAST)</a>, R Overbeek, R Olson, GD Pusch, GJ Olsen, JJ Davis… - Nucleic acids research, 2014</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=11459795107617940916&btnI=1&hl=en" target="blank">PATRIC, the bacterial bioinformatics database and analysis resource</a>, AR Wattam, D Abraham, O Dalay, TL Disz, T Driscoll… - Nucleic acids research, 2014</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=10073701961013691123&btnI=1&hl=en" target="blank">Comparative genomic analysis at the PATRIC, a bioinformatic resource center</a>, AR Wattam, JL Gabbard, M Shukla, BW Sobral - Host-Bacteria Interactions, 2014</p>
+
+        <hr />
+        <h6>2012</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=7403158439422631264&btnI=1&hl=en" target="blank">EuPathDB: the eukaryotic pathogen database</a>, C Aurrecoechea, A Barreto, J Brestelli, BP Brunk… - Nucleic acids research, 2012</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=4808645706162772436&btnI=1&hl=en" target="blank">FungiDB: an integrated functional genomics database for fungi</a>, JE Stajich, T Harris, BP Brunk, J Brestelli, S Fischer… - Nucleic acids research, 2012</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=13680095123055813057&btnI=1&hl=en" target="blank">VectorBase: improvements to a bioinformatics resource for invertebrate vector genomics</a>, K Megy, SJ Emrich, D Lawson, D Campbell, E Dialynas… - Nucleic acids research, 2012</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=18004330097385029229&btnI=1&hl=en" target="blank">ViPR: an open bioinformatics database and analysis resource for virology research</a>, BE Pickett, EL Sadat, Y Zhang, JM Noronha… - Nucleic acids research, 2012</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=163333017779699525&btnI=1&hl=en" target="blank">Virus pathogen database and analysis resource (ViPR): a comprehensive bioinformatics database and analysis resource for the coronavirus research community</a>, BE Pickett, DS Greer, Y Zhang, L Stewart, L Zhou… - Viruses, 2012</p>
+
+        <hr />
+        <h6>2011</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=7525689214373243628&btnI=1&hl=en" target="blank">PATRIC: the comprehensive bacterial bioinformatics resource with a focus on human pathogenic species</a>, JJ Gillespie, AR Wattam, SA Cammer, JL Gabbard… - Infection and immunity, 2011</p>
+
+        <hr />
+        <h6>2010</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=183505695796929596&btnI=1&hl=en" target="blank">AmoebaDB and MicrosporidiaDB: functional genomic resources for Amoebozoa and Microsporidia species</a>, C Aurrecoechea, A Barreto, J Brestelli, BP Brunk… - Nucleic acids research, 2010</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=2175663199984173998&btnI=1&hl=en" target="blank">EuPathDB: a portal to eukaryotic pathogen databases</a>, C Aurrecoechea, J Brestelli, BP Brunk, S Fischer… - Nucleic acids research, 2010</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=425498886008659265&btnI=1&hl=en" target="blank">TriTrypDB: a functional genomic resource for the Trypanosomatidae</a>, M Aslett, C Aurrecoechea, M Berriman, J Brestelli… - Nucleic acids research, 2010</p>
+
+        <hr />
+        <h6>2009</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=1793531804578580553&btnI=1&hl=en" target="blank">GiardiaDB and TrichDB: integrated genomic resources for the eukaryotic protist pathogens Giardia lamblia and Trichomonas vaginalis</a>, C Aurrecoechea, J Brestelli, BP Brunk, JM Carlton… - Nucleic acids research, 2009</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=4544588571488155873&btnI=1&hl=en" target="blank">PlasmoDB: a functional genomic database for malaria parasites</a>, C Aurrecoechea, J Brestelli, BP Brunk, J Dommer… - Nucleic acids research, 2009</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=15308061348194079388&btnI=1&hl=en" target="blank">VectorBase: a data resource for invertebrate vector genomics</a>, D Lawson, P Arensburger, P Atkinson, NJ Besansky… - Nucleic acids research, 2009</p>
+
+        <hr />
+        <h6>2008</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=2975886871571279584&btnI=1&hl=en" target="blank">The RAST Server: rapid annotations using subsystems technology</a>, RK Aziz, D Bartels, AA Best, M DeJongh, T Disz… - BMC genomics, 2008</p>
+
+        <hr />
+        <h6>2007</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=14044823992276339338&btnI=1&hl=en" target="blank">ApiDB: integrated resources for the apicomplexan bioinformatics resource center</a>, C Aurrecoechea, M Heiges, H Wang, Z Wang… - Nucleic acids research, 2007</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=1749858721012160105&btnI=1&hl=en" target="blank">PATRIC: the VBI pathosystems resource integration center</a>, EE Snyder, N Kampanya, J Lu, EK Nordberg, HR Karur… - Nucleic acids research, 2007</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=7579519775418541255&btnI=1&hl=en" target="blank">ToxoDB: an integrated Toxoplasma gondii database resource</a>, B Gajria, A Bahl, J Brestelli, J Dommer, S Fischer… - Nucleic acids research, 2007</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=34197150635581884&btnI=1&hl=en" target="blank">VectorBase: a home for invertebrate vectors of human pathogens</a>, D Lawson, P Arensburger, P Atkinson, NJ Besansky… - Nucleic acids research, 2007</p>
+
+        <hr />
+        <h6>2006</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=5736630799283119844&btnI=1&hl=en" target="blank">CryptoDB: a Cryptosporidium bioinformatics resource update</a>, M Heiges, H Wang, E Robinson, C Aurrecoechea… - Nucleic acids research, 2006</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=911196506500602112&btnI=1&hl=en" target="blank">OrthoMCL-DB: querying a comprehensive multi-species collection of ortholog groups</a>, F Chen, AJ Mackey, CJ Stoeckert Jr, DS Roos - Nucleic acids research, 2006</p>
+
+        <hr />
+        <h6>2004</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=2245819101385296480&btnI=1&hl=en" target="blank">CryptoDB: the Cryptosporidium genome resource</a>, D Puiu, S Enomoto, GA Buck, MS Abrahamsen… - Nucleic acids research, 2004</p>
+
+        <hr />
+        <h6>2003</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=9816247536791701475&btnI=1&hl=en" target="blank">OrthoMCL: identification of ortholog groups for eukaryotic genomes</a>, L Li, CJ Stoeckert, DS Roos - Genome research, 2003</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=3361131538240750547&btnI=1&hl=en" target="blank">PlasmoDB: the Plasmodium genome resource. A database integrating experimental and computational data</a>, A Bahl, B Brunk, J Crabtree, MJ Fraunholz, B Gajria… - Nucleic acids research, 2003</p>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=13772246464042495892&btnI=1&hl=en" target="blank">ToxoDB: accessing the Toxoplasma gondii genome</a>, JC Kissinger, B Gajria, L Li, IT Paulsen, DS Roos - Nucleic acids research, 2003</p>
+
+        <hr />
+        <h6>2002</h6>
+        <p><a href="https://scholar.google.com/scholar?oi=bibs&cluster=1508914291843804256&btnI=1&hl=en" target="blank">PlasmoDB: the Plasmodium genome resource. An integrated database providing tools for accessing, analyzing and mapping expression and sequence data (both finished and unfinished)</a>, A Bahl, B Brunk, RL Coppel, J Crabtree, SJ Diskin… - Nucleic Acids Research, 2002</p>
+
       </div>
     </div>
   </Layout>
