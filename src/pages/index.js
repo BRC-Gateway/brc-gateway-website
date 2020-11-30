aad2259bb44c7b260219ba4570fe1210712f206b
Chris <chris.c.thomas@icloud.com>
2020-11-30 11:45:07 -0600

    Restyle contract box

--- src/pages/index.js
+++ src/pages/index.js
@@ -207,17 +207,21 @@ const IndexPage = () => (
       </ul>
     </div>
 
-    <div class="row mt-5">
+<div className="bg-wash tw-border-b-4 tw-border-gray-600">
+              <div class="row mt-5">
       <div class="col-md-12">
         <p style={{
           position: 'relative',
           zIndex: '100',
+          padding: '20px',
         }}>These projects have been funded in whole or in part with Federal funds from the National Institute of Allergy and Infectious Diseases, National Institutes of Health, Department of Health and Human Services:</p>
       </div>
     </div>
-    <div class="row mt-2 mb-5">
+
+    <div class="row mb-5">
+
       <div class="col-md-6 mb-4 mb-md-0">
-        <div class="bg-wash p-3 pb-0 mb-4 tw-border-b-4 tw-border-gray-600 h-100">
+        <div class="bg-wash p-3 pb-0 h-100">
           <h5 class="tw-mb-1"><a href="https://www.bv-brc.org" target="_blank">BV-BRC</a></h5>
           <p>Contract No. 75N93019C00076, awarded to the University of Chicago</p>
           <div class="social-buttons">
@@ -236,7 +240,7 @@ const IndexPage = () => (
         </div>
       </div>
       <div class="col-md-6">
-        <div class="bg-wash p-3 mb-4 tw-border-b-4 tw-border-gray-600 h-100">
+        <div class="bg-wash p-3 h-100">
           <h5 class="tw-mb-1"><a href="https://veupathdb.org/veupathdb/app/" target="_blank">VEuPathDB</a></h5>
           <p>Contract No. HHSN75N93019C00077, with additional support from the Wellcome Trust (Resource Grants 212929 & 218288)</p>
           <div class="social-buttons">
@@ -255,6 +259,9 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
+
+</div>
+
 
   </Layout >
 )
