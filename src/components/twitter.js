1a4d2571a26696d944d361a9e63b3800c64b35e8
Chris <chris.c.thomas@icloud.com>
2020-11-30 10:52:22 -0600

    Update Twitter styles

--- src/components/twitter.js
+++ src/components/twitter.js
@@ -2,7 +2,8 @@ import React from "react"
 
 const Twitter = () => (
   <>
-    <a class="twitter-timeline" data-height="600" href="https://twitter.com/BrcGateway/lists/brc-gateway?ref_src=twsrc%5Etfw">A Twitter List by BRCGateway</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <h5>Twitter List by <a href="https://twitter.com/i/lists/1319290212527165440">BRCGateway</a></h5>
+    <a class="twitter-timeline" data-height="600" data-chrome="noheader nofooter" href="https://twitter.com/BrcGateway/lists/brc-gateway?ref_src=twsrc%5Etfw">A Twitter List by BRCGateway</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   </>
 )
 
